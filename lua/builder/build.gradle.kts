99a03b15df912f248cc7152a28cf919f9c49424c
Natan <xpenatan@gmail.com>
2023-12-16 19:53:13 -0300

    build lua dll

--- lua/builder/build.gradle.kts
+++ lua/builder/build.gradle.kts
@@ -28,18 +28,44 @@ val zippedPath = "$buildDir/lua-source.zip"
 val sourcePath = "$buildDir/lua-source"
 val sourceDestination = "$buildDir/lua/"
 
-tasks.register<Download>("download_source") {
+tasks.register<Download>("download_source_lua") {
     group = "lua"
     description = "Download lua source"
-    src("https://github.com/lua/lua/archive/master.zip")
+    src("https://github.com/lua/lua/archive/refs/tags/v5.4.4.zip")
     dest(File(zippedPath))
     doLast {
         unzipTo(File(sourcePath), dest)
         copy{
-            from("$sourcePath/lua-master")
+            from("$sourcePath/lua-5.4.4")
             into(sourceDestination)
         }
         delete(sourcePath)
         delete(zippedPath)
     }
+}
+
+val solZippedPath = "$buildDir/sol2-source.zip"
+val solSourcePath = "$buildDir/sol2-source"
+val solSourceDestination = "$buildDir/sol2/"
+
+tasks.register<Download>("download_source_sol") {
+    group = "lua"
+    description = "Download sol2 source"
+    src("https://github.com/ThePhD/sol2/archive/refs/tags/v3.3.1.zip")
+    dest(File(solZippedPath))
+    doLast {
+        unzipTo(File(solSourcePath), dest)
+        copy{
+            from("$solSourcePath/sol2-3.3.1")
+            into(solSourceDestination)
+        }
+        delete(solSourcePath)
+        delete(solZippedPath)
+    }
+}
+
+tasks.register("download_source") {
+    group = "lua"
+    description = "Download source"
+    dependsOn(arrayOf("download_source_sol", "download_source_lua"))
 }
--- lua/builder/src/main/java/Main.java
+++ lua/builder/src/main/java/Main.java
@@ -5,29 +5,20 @@ import com.github.xpenatan.jparser.builder.JBuilder;
 import com.github.xpenatan.jparser.builder.targets.WindowsTarget;
 import com.github.xpenatan.jparser.core.util.FileHelper;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        generate();
+        buildLua();
+//        buildSol();
     }
 
-    public static void generate() throws Exception {
-        String basePackage = "lua";
-        String cppSourceDir = new File("./build/lua/").getCanonicalPath();
-        String baseJavaDir = new File(".").getAbsolutePath() + "./base/src/main/java";
-
-        generateAndBuild(basePackage, baseJavaDir, cppSourceDir);
-    }
-
-    private static void generateAndBuild(
-            String basePackage,
-            String baseJavaDir,
-            String cppSourceDir
-    ) throws Exception {
+    private static void buildLua() throws Exception {
         String libName = "lua";
 
+        String cppSourceDir = new File("./build/lua/").getCanonicalPath();
         String libsDir = new File("./build/c++/libs/").getCanonicalPath();
         String libBuildPath = new File("./build/c++/").getCanonicalPath();
         String cppDestinationPath = libBuildPath + "/src";
@@ -51,15 +42,92 @@ public class Main {
         JBuilder.build(buildConfig, targets);
     }
 
-    private static BuildMultiTarget getStaticLuaWindowBuildTarget() {
+    private static BuildMultiTarget getStaticLuaWindowBuildTarget() throws IOException {
+        String buildPath = new File("build/c++/").getCanonicalPath().replace("\\", "/");
         BuildMultiTarget multiTarget = new BuildMultiTarget();
 
         WindowsTarget windowsTarget = new WindowsTarget();
+        windowsTarget.cppCompiler.clear();
+        windowsTarget.linkerCompiler.clear();
+        windowsTarget.cppCompiler.add("x86_64-w64-mingw32-gcc");
+        windowsTarget.linkerCompiler.add("x86_64-w64-mingw32-gcc");
         windowsTarget.isStatic = true;
         windowsTarget.addJNI = false;
         windowsTarget.headerDirs.add("-Isrc/lua/");
         windowsTarget.filterCPPSuffix = ".c";
-        windowsTarget.cppIncludes.add("**/lua/*.c");
+        windowsTarget.cppInclude.add("**/lua/*.c");
+        windowsTarget.cppExclude.add("**/lua/lua.c");
+        windowsTarget.cppExclude.add("**/lua/ltests.c");
+        windowsTarget.cppExclude.add("**/lua/onelua.c");
+        windowsTarget.cppFlags.add("-std=gnu99");
+        windowsTarget.cppFlags.add("-DLUA_BUILD_AS_DLL");
+        windowsTarget.cppFlags.add("-DLUA_COMPAT_5_3");
+        windowsTarget.cppFlags.remove("-std=c++17");
+        multiTarget.add(windowsTarget);
+
+        WindowsTarget linkTarget = new WindowsTarget();
+        linkTarget.cppCompiler.clear();
+        linkTarget.linkerCompiler.clear();
+        linkTarget.cppCompiler.add("x86_64-w64-mingw32-gcc");
+        linkTarget.linkerCompiler.add("x86_64-w64-mingw32-gcc");
+        linkTarget.headerDirs.add("-Isrc/lua/");
+        linkTarget.filterCPPSuffix = ".c";
+        linkTarget.shouldLink = true;
+        linkTarget.shouldCompile = true;
+        linkTarget.cppInclude.add("**/lua/lua.c");
+        linkTarget.addJNI = false;
+        linkTarget.linkerFlags.add(buildPath + "/libs/windows/lua64.a");
+        linkTarget.cppFlags.remove("-std=c++17");
+        multiTarget.add(linkTarget);
+
+        return multiTarget;
+    }
+
+    private static void buildSol() throws Exception {
+        String libName = "sol";
+
+        String cppSourceDir = new File("./build/sol2/").getCanonicalPath();
+        String libsDir = new File("./build/c++/libs/").getCanonicalPath();
+        String libBuildPath = new File("./build/c++/").getCanonicalPath();
+        String cppDestinationPath = libBuildPath + "/src";
+        String libDestinationPath = cppDestinationPath + "/sol2";
+
+        FileHelper.copyDir(cppSourceDir, libDestinationPath);
+
+        BuildConfig buildConfig = new BuildConfig(
+                cppDestinationPath,
+                libBuildPath,
+                libsDir,
+                libName
+        );
+
+        ArrayList<BuildMultiTarget> targets = new ArrayList<>();
+
+        if(BuildTarget.isWindows() || BuildTarget.isUnix()) {
+            targets.add(getSolWindowBuildTarget());
+        }
+
+        JBuilder.build(buildConfig, targets);
+    }
+
+    private static BuildMultiTarget getSolWindowBuildTarget() throws IOException {
+        BuildMultiTarget multiTarget = new BuildMultiTarget();
+
+        String buildPath = new File("build/c++/").getCanonicalPath().replace("\\", "/");
+        String libPath = buildPath + "/libs/windows";
+
+        WindowsTarget windowsTarget = new WindowsTarget();
+        windowsTarget.isStatic = true;
+        windowsTarget.addJNI = false;
+        windowsTarget.headerDirs.add("-Isrc/sol2/include");
+        windowsTarget.headerDirs.add("-Isrc/lua/");
+        windowsTarget.filterCPPSuffix = ".hpp";
+        windowsTarget.cppInclude.add("**/sol2/include/**.hpp");
+
+
+        windowsTarget.linkerFlags.add("-L" + libPath);
+//        windowsTarget.linkerFlags.add("-l:lua64.a");
+        windowsTarget.linkerFlags.add("-l:liblua-5.4.4.dll");
         multiTarget.add(windowsTarget);
         return multiTarget;
     }
