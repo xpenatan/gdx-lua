300dd1f7c88e07bdf2fa7ed15bb9c467e15677dd
Natan <xpenatan@gmail.com>
2024-01-04 14:06:53 -0300

    remove clean task

--- lua/lua-core/build.gradle.kts
+++ lua/lua-core/build.gradle.kts
@@ -1,7 +1,5 @@
 val moduleName = "lua-core"
 
-val includePom = configurations.create("includePom")
-includePom.extendsFrom(configurations.implementation.get())
 val genPath = File("$projectDir/../lua-gen/src/main/java")
 println("Path " + genPath.canonicalPath)
 sourceSets["main"].java {
@@ -10,13 +8,6 @@ sourceSets["main"].java {
 
 dependencies {
     implementation("com.github.xpenatan.jParser:loader-core:${LibExt.jParserVersion}")
-}
-
-tasks.named("clean") {
-    doFirst {
-        val srcPath = "$projectDir/src/main/java"
-        project.delete(files(srcPath))
-    }
 }
 
 java {
